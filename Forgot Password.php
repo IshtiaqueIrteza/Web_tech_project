26cad58010b149f4bc9a3b1c0b0c4ce480a1124b
sajidhasan054 <sajidhasan054@gmail.com>
2017-12-22 23:00:44 +0600

    First prototype

---
+++ Forgot Password.php
@@ -0,0 +1,31 @@
+<html>
+	<body>
+		<table border="1" height="70%" width="80%" cellpadding="20">
+			<tr height="20%">
+				<td>
+					<img align="center" src="Logo.png"/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+					&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+					&emsp;&emsp;&emsp;
+					<a href="Home.php"/>Home | </a>
+					<a href="Login.php"/>Login | </a>
+					<a href="Registration.php"/>Registration </a>
+			</tr>
+			<tr height="200%">
+				<td>
+					<form>
+						<fieldset>
+							<legend>FORGOT PASSWORD</legend>
+							Enter Email:<input name="email" type="text"/><hr>
+							<input type="submit"/>
+						</fieldset>
+					</form>
+				</td>
+			</tr>
+			<tr height="10%">
+				<td align="center">
+					Copyright © 2017
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
---
+++ Logged In.php
@@ -0,0 +1,40 @@
+<html>
+	<form>
+		<body>
+			<table border="1" height="70%" width="100%" cellpadding="20">
+				<tr>
+					<td colspan="1" align="center"> <a href="Dashboard.php"> Profile </a> </td>
+					<td colspan="10" valign="top">
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;
+						<input type="button" value="Upload"><br><br>
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						&emsp;&emsp;&emsp;&emsp;&emsp;
+						<input type="button" value="Delete"><br>
+						&emsp;&emsp;&emsp;
+						<img src="img 1.jpg" width="400" height="300"/>
+						&emsp;&emsp;&emsp;
+						<img src="img 2.jpeg" width="400" height="300"/> <br>
+						&emsp;&emsp;&emsp;
+						<input type="button" value="Like"/>
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&ensp;&ensp;
+						<input type="button" value="Like"/> <br><br>
+						&emsp;&emsp;&emsp;
+						<input type="text" value="Write a comment"/>
+						&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+						<input type="text" value="Write a comment"/>
+					</td>
+					<td colspan="1">
+					<a href="Contest.php">Contest</a>
+					</td>
+				</tr>
+			</table>
+		</body>
+	</form>
+</html>
