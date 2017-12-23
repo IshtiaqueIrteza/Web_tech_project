71f480b747b3ec9a07f9748280fd8bb018802e95
nushrat57 <nushrat57@gmail.com>
2017-12-23 17:42:07 +0600

    prototype
    These are prototypes

---
+++ Home.php
@@ -0,0 +1,26 @@
+<html>
+	<body>
+		<table border="1" height="70%" width="80%" cellpadding="20">
+			<tr height="20%">
+				<td>
+					<img align="center" src="Logo.png"/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+					&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+					&emsp;&emsp;&emsp;
+					<a href="Home.php"/>Home | </a>
+					<a href="Login.php"/>Login | </a>
+					<a href="Registration.php"/>Registration </a>
+				</td>
+			</tr>
+			<tr height="200%">
+				<td>
+					<h3>Welcome to xCompany</h3>
+				</td>
+			</tr>
+			<tr height="10%">
+				<td align="center">
+					Copyright © 2017
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
